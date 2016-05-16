722c68dabc7226da78f12ffcbdbab5b18892e48d
Mike Davis <mike@lightcycle.org>
2016-05-15 22:00:04 -0500

    Refactoring - extracted functions for readability

--- Example-PhotoOrientation/dataset_generator.py
+++ Example-PhotoOrientation/dataset_generator.py
@@ -8,34 +8,55 @@ import tempfile
 import random
 from PIL import Image
 
-imagecount = 100
-training_output_dir = "training"
 mincolors = 1024
-rotations = {0: '1000', 90: '0100', 180: '0010', 270: '0001'}
+output_size = (400, 400)
 
-if not os.path.exists(training_output_dir):
-    os.makedirs(training_output_dir)
+
+def random_image_urls():
+    page = wikipedia.page(wikipedia.random())
+    return [url for url in page.images if url.endswith(".png") or url.endswith(".jpg")]
+
+
+def load_image(url):
+    file = cStringIO.StringIO(urllib.urlopen(url).read())
+    return Image.open(file)
+
+
+def random_rotation():
+    rotation_labels = {0: '1000', 90: '0100', 180: '0010', 270: '0001'}
+    rotation = random.randint(0, 3) * 90
+    return rotation, rotation_labels[rotation]
+
+
+def usable_image(image):
+    return image.mode == "RGB" and image.size >= output_size and image.getcolors(mincolors) is None
+
+
+def get_formatted_image(image):
+    image.thumbnail(output_size)
+    new_image = Image.new("RGB", output_size)
+    new_image.paste(image, ((new_image.size[0] - image.size[0]) / 2, (new_image.size[1] - image.size[1]) / 2))
+    return new_image
+
+
+def create_dataset(dir, num):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    while num > 0:
+        try:
+            for url in random_image_urls():
+                image = load_image(url)
+                if usable_image(image):
+                    angle, label = random_rotation()
+                    tf = tempfile.NamedTemporaryFile(suffix = "-" + label + ".jpg", prefix = "", dir = dir, delete = False)
+                    get_formatted_image(image).rotate(angle).save(tf.name)
+                    num -= 1
+        except Exception as e:
+            logging.error(traceback.format_exc())
+
 
 random.seed()
-
 wikipedia.set_lang("en")
-
-while imagecount > 0:
-    try:
-        page = wikipedia.page(wikipedia.random())
-        imageurls = [url for url in page.images if url.endswith(".png") or url.endswith(".jpg")]
-        for imageurl in imageurls:
-            file = cStringIO.StringIO(urllib.urlopen(imageurl).read())
-            image = Image.open(file)
-            width, height = image.size
-            has_many_colors = image.getcolors(mincolors) is None
-            if image.mode == "RGB" and width > 400 and height > 400 and has_many_colors:
-                rotation = random.randint(0, len(rotations) - 1)
-                tf = tempfile.NamedTemporaryFile(suffix = "-" + rotations.values()[rotation] + ".jpg", prefix = "", dir = training_output_dir, delete = False)
-                image.thumbnail((400,400))
-                new_image = Image.new("RGB", (400,400))
-                new_image.paste(image, ((new_image.size[0] - image.size[0]) / 2, (new_image.size[1] - image.size[1]) / 2))
-                new_image.rotate(rotations.keys()[rotation]).save(tf.name)
-                imagecount -= 1
-    except Exception as e:
-        logging.error(traceback.format_exc())
+create_dataset("train", 10)
+create_dataset("test", 10)
