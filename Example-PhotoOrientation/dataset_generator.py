5bee733732a8e86e51000f5cf3e782aad0199dfe
Mike Davis <mike@lightcycle.org>
2016-05-16 20:03:13 -0500

    Pull images from wikimedia commons instead of wikipedia

--- Example-PhotoOrientation/dataset_generator.py
+++ Example-PhotoOrientation/dataset_generator.py
@@ -1,35 +1,34 @@
-import wikipedia
-import urllib
 import cStringIO
 import os
 import traceback
 import logging
 import tempfile
 import random
+import mwclient
 from PIL import Image
 
-mincolors = 1024
-output_size = (400, 400)
+
+def random_image_title():
+    for title in (page['title'] for page in site.random('6', limit = None)):
+        if title.lower().endswith((".png", "jpg")):
+            yield title
 
 
-def random_image_urls():
-    page = wikipedia.page(wikipedia.random())
-    return [url for url in page.images if url.endswith(".png") or url.endswith(".jpg")]
+def random_image():
+    for title in random_image_title():
+        data = cStringIO.StringIO()
+        mwclient.image.Image(site, title).download(data)
+        yield Image.open(data)
 
 
-def load_image(url):
-    file = cStringIO.StringIO(urllib.urlopen(url).read())
-    return Image.open(file)
+def usable_image(image):
+    return image.mode == "RGB" and image.size >= output_size and image.getcolors(mincolors) is None
 
 
 def random_rotation():
     rotation_labels = {0: '1000', 90: '0100', 180: '0010', 270: '0001'}
     rotation = random.randint(0, 3) * 90
     return rotation, rotation_labels[rotation]
-
-
-def usable_image(image):
-    return image.mode == "RGB" and image.size >= output_size and image.getcolors(mincolors) is None
 
 
 def get_formatted_image(image):
@@ -43,20 +42,24 @@ def create_dataset(dir, num):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-    while num > 0:
+    images = random_image()
+    while len(os.listdir(dir)) < num:
         try:
-            for url in random_image_urls():
-                image = load_image(url)
-                if usable_image(image):
-                    angle, label = random_rotation()
-                    tf = tempfile.NamedTemporaryFile(suffix = "-" + label + ".jpg", prefix = "", dir = dir, delete = False)
-                    get_formatted_image(image).rotate(angle).save(tf.name)
-                    num -= 1
+            image = next(images)
+            if usable_image(image):
+                angle, label = random_rotation()
+                tf = tempfile.NamedTemporaryFile(suffix = "-" + label + ".jpg", prefix = "", dir = dir, delete = False)
+                get_formatted_image(image).rotate(angle).save(tf.name)
+        except StopIteration:
+            images = random_image()
         except Exception as e:
             logging.error(traceback.format_exc())
 
 
+site = mwclient.client.Site('commons.wikimedia.org')
+mincolors = 10000
+output_size = (400, 400)
 random.seed()
-wikipedia.set_lang("en")
-create_dataset("train", 10000)
-create_dataset("test", 10000)
+
+create_dataset("./train", 10000)
+create_dataset("./test", 10000)
