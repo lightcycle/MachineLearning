a6622ec985d7af2631a3fc1be4f228e5ed46d69f
Mike Davis <mike@lightcycle.org>
2016-09-04 13:47:03 -0500

    Add method for getting batches without shuffling. General cleanup.

--- Example-PhotoOrientation/DatasetLoader.py
+++ Example-PhotoOrientation/DatasetLoader.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 class DatasetLoader:
 
-    @staticmethod
-    def __file_reader(filename_queue):
+    @classmethod
+    def __file_reader(cls, filename_queue):
         reader = tf.TFRecordReader()
         _, serialized_example = reader.read(filename_queue)
         features = tf.parse_single_example(
@@ -14,15 +14,26 @@ class DatasetLoader:
                 'label': tf.FixedLenFeature([4], tf.int64)
             })
         image = tf.image.decode_jpeg(features['image'], channels=3)
-        # TODO set width and height in TFRecords file
-        image.set_shape([100, 100, 3])
+        image.set_shape([200, 200, 3]) # TODO: find a workaround using array_ops
         label = features['label']
         return image, label
 
-    def input_pipeline(self, filenames, batch_size, read_threads, num_epochs = None, min_after_dequeue = 10000):
-        filename_queue = tf.train.string_input_producer(filenames, num_epochs = num_epochs, shuffle = True)
-        readers = [self.__file_reader(filename_queue) for _ in range(read_threads)]
+    @classmethod
+    def threaded_readers(cls, filenames, read_threads, num_epochs = None, shuffle = False):
+        filename_queue = tf.train.string_input_producer(filenames, num_epochs = num_epochs, shuffle = shuffle)
+        return [cls.__file_reader(filename_queue) for _ in range(read_threads)]
+
+    @classmethod
+    def input_shuffle_batch(cls, filenames, batch_size, read_threads, num_epochs = None, min_after_dequeue = 10000):
+        readers = cls.threaded_readers(filenames, read_threads, num_epochs, True)
         capacity = min_after_dequeue + 3 * batch_size
         image_batch, label_batch = tf.train.shuffle_batch_join(
             readers, batch_size = batch_size, capacity = capacity, min_after_dequeue = min_after_dequeue)
         return image_batch, label_batch
+
+    @classmethod
+    def input_batch(cls, filenames, batch_size, read_threads, num_epochs=None):
+        readers = cls.threaded_readers(filenames, read_threads, num_epochs)
+        image_batch, label_batch = tf.train.batch_join(readers, batch_size = batch_size)
+        return image_batch, label_batch
+
