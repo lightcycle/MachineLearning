5cbdee552d9a7179a15cd76d7c8567e5af06eb74
Mike Davis <mike@lightcycle.org>
2016-09-04 13:55:40 -0500

    Use TF operations to get image and label batches. Expect 200x200 RGB images. Add optional CPU profiling.

--- Example-PhotoOrientation/train_cnn.py
+++ Example-PhotoOrientation/train_cnn.py
@@ -1,59 +1,94 @@
-import shutil
 import tensorflow as tf
-import operator
+from tensorflow.python.client import timeline
+import multiprocessing
 import os
+import shutil
 from glob import glob
-from dataset_loader import get_dataset_batcher, load_image
-from itertools import compress
-from scipy.misc import imsave
-import model
+from DatasetLoader import DatasetLoader
+from Model import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('train_steps', 10000, 'number of steps to run trainer')
-flags.DEFINE_integer('update_steps', 100, 'interval at which to print step count with current accuracy')
-flags.DEFINE_integer('batch_size', 50, 'batch size')
-flags.DEFINE_string('train_dir', './train', 'directory containing training data')
-flags.DEFINE_string('test_dir', './test', 'directory containing training data')
+flags.DEFINE_integer('training_epochs', 30, 'number of times to run through training dataset')
+flags.DEFINE_integer('batch_size', 100, 'batch size')
+flags.DEFINE_string('train_files_glob', './input/train*.tfrecords', 'glob for TFRecords files containing training data')
+flags.DEFINE_string('test_files_glob', './input/test*.tfrecords', 'glob for TFRecords files containing testing data')
 flags.DEFINE_string('model_file', './model.ckpt', 'path to save or load trained model parameters')
 flags.DEFINE_boolean('train', True, 'whether or not to train model')
 flags.DEFINE_boolean('test', True, 'whether or not to test model')
-flags.DEFINE_boolean('save_test_examples', False, 'whether or not to save examples of passing and failing model input')
-flags.DEFINE_integer('num_passing_test_examples', 10, 'number of examples of passing model input to save')
-flags.DEFINE_integer('num_failing_test_examples', 10, 'number of examples of failing model input to save')
-flags.DEFINE_string('examine_file', None, 'path to a specific image input to save detailed results for')
+flags.DEFINE_integer('read_threads', multiprocessing.cpu_count(), 'number of reading threads')
+flags.DEFINE_string('profile_training_batch', 'trace.json', 'if set, a Chrome trace file will be written at the specified path for the first training batch')
 
-image_size = (200, 200)
-
-y_ = tf.placeholder(tf.float32, [None, 4])
-x, y_conv, keep_prob, h_conv1, b_conv1, h_conv2, b_conv2 = model.create_graph(image_size)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-saver = tf.train.Saver()
-
+dataset_loader = DatasetLoader()
 
 def recreate(dir):
     if os.path.exists(dir):
         shutil.rmtree(dir)
     os.makedirs(dir)
 
+image_batch_holder = tf.placeholder(tf.uint8, shape = (None, 200, 200, 3))
+label_batch_holder = tf.placeholder(tf.int64, shape = (None, 4))
+keep_prob_holder = tf.placeholder(tf.float32, shape = ())
+
+inferred_labels = Model.create_graph(image_batch_holder, keep_prob_holder)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(tf.cast(label_batch_holder, tf.float32) * tf.log(inferred_labels), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(inferred_labels, 1), tf.argmax(tf.cast(label_batch_holder, tf.float32), 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver()
+
+
+def trace_options(trace_path):
+    if trace_path is not None:
+        return tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE), tf.RunMetadata()
+    else:
+        return None, None
+
 
 with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
 
     if FLAGS.train:
         print "Starting training."
-        dataset_batcher = get_dataset_batcher(glob(FLAGS.train_dir + "/*.jpg"), batch_size = FLAGS.batch_size, scale = image_size, repeat = True)
-        for i in range(FLAGS.train_steps):
-            train_images, train_labels = dataset_batcher.next()
-            if i > 0 and i % (FLAGS.update_steps) == 0:
-                train_accuracy = accuracy.eval(session=sess, feed_dict={x: train_images, y_: train_labels, keep_prob: 1.0})
-                print("\tstep %d, training accuracy %g"%(i, train_accuracy))
-            train_step.run(session=sess, feed_dict={x: train_images, y_: train_labels, keep_prob: 0.5})
-        print "Training complete, saving model."
+
+        image_batch, label_batch = dataset_loader.input_shuffle_batch(
+            glob(FLAGS.train_files_glob), FLAGS.batch_size, FLAGS.read_threads, num_epochs=FLAGS.training_epochs)
+        label_batch = tf.cast(label_batch, tf.float32)
+
+        sess.run(tf.initialize_all_variables())
+
+        writer = tf.train.SummaryWriter("./tensorboard", sess.graph)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        image_batch_eval = image_batch.eval()
+        label_batch_eval = label_batch.eval()
+
+        try:
+            while not coord.should_stop():
+                run_options, run_metadata = trace_options(FLAGS.profile_training_batch)
+                sess.run(train_step, feed_dict = {
+                    image_batch_holder: image_batch_eval,
+                    label_batch_holder: label_batch_eval,
+                    keep_prob_holder: 0.5
+                }, options = run_options, run_metadata = run_metadata)
+                writer.add_summary(sess.run(tf.merge_all_summaries()))
+                if FLAGS.profile_training_batch is not None:
+                    with open(FLAGS.profile_training_batch, 'w') as f:
+                        f.write(timeline.Timeline(run_metadata.step_stats).generate_chrome_trace_format())
+                    FLAGS.profile_training_batch = None
+                print 'Finished batch'
+        except tf.errors.OutOfRangeError:
+            print('Training complete -- epoch limit reached')
+        finally:
+            coord.request_stop()
+
+        coord.join(threads)
+        writer.close()
+
+        print "Saving model."
         save_path = saver.save(sess, FLAGS.model_file)
         print "Model saved."
     else:
@@ -63,42 +98,34 @@ with tf.Session() as sess:
 
     if FLAGS.test:
         print "Starting test."
+
+        image_batch, label_batch = dataset_loader.input_batch(
+            glob(FLAGS.test_files_glob), FLAGS.batch_size, FLAGS.read_threads, num_epochs=FLAGS.training_epochs)
+        label_batch = tf.cast(label_batch, tf.float32)
+
+        sess.run(tf.initialize_all_variables())
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        image_batch_eval = image_batch.eval()
+        label_batch_eval = label_batch.eval()
+
         scores = list()
-        dataset_batcher = get_dataset_batcher(glob(FLAGS.test_dir + "/*.jpg"), batch_size = FLAGS.batch_size, scale = image_size, repeat = False)
-        for test_images, test_labels in dataset_batcher:
-            score = accuracy.eval(session=sess, feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0})
-            print("\ttest accuracy %g" % score)
-            scores.append(score)
-        print "Overall test accuracy %g" % (reduce(lambda x, y: x + y, scores) / len(scores))
+        try:
+            while not coord.should_stop():
+                score = sess.run(accuracy, feed_dict={
+                    image_batch_holder: image_batch_eval,
+                    label_batch_holder: label_batch_eval,
+                    keep_prob_holder: 1
+                })
+                print("\ttest accuracy %g" % score)
+                scores.append(score)
+            print "Overall test accuracy %g" % (reduce(lambda x, y: x + y, scores) / len(scores))
 
-    if FLAGS.save_test_examples:
-        recreate("examples")
-        pass_count = FLAGS.num_passing_test_examples
-        fail_count = FLAGS.num_passing_test_examples
-        dataset_batcher = get_dataset_batcher(glob(FLAGS.test_dir + "/*.jpg"), batch_size=FLAGS.batch_size,
-                                              scale=image_size, repeat=False)
-        while pass_count > 0 and fail_count > 0:
-            test_images, test_labels = dataset_batcher.next()
-            correct = correct_prediction.eval(session=sess, feed_dict={
-                x: test_images, y_: test_labels, keep_prob: 1.0})
-            for pass_image in compress(test_images, correct):
-                if pass_count > 0:
-                    imsave("examples/passed_" + str(pass_count) + ".jpg", pass_image)
-                    pass_count -= 1
-            for fail_image in compress(test_images, map(operator.not_, correct)):
-                if fail_count > 0:
-                    imsave("examples/failed_" + str(fail_count) + ".jpg", fail_image)
-                    fail_count -= 1
+        except tf.errors.OutOfRangeError:
+            print('Testing complete -- epoch limit reached')
+        finally:
+            coord.request_stop()
 
-    if FLAGS.examine_file:
-        recreate("examine")
-        image = load_image(FLAGS.examine_file)
-        images = image.reshape(-1, image.shape[0], image.shape[1], image.shape[2])
-        h1 = h_conv1.eval(session = sess, feed_dict={x: images})
-        bias1 = b_conv1.eval(session = sess)
-        for i in range(0, len(h1[0,0,0])):
-            imsave("examine/h1_" + str(bias1[i]) + ".jpg", h1[0,slice(None),slice(None),i])
-        h2 = h_conv2.eval(session=sess, feed_dict={x: images})
-        bias2 = b_conv2.eval(session=sess)
-        for i in range(0, len(h2[0, 0, 0])):
-            imsave("examine/h2_" + str(bias2[i]) + ".jpg", h2[0, slice(None), slice(None), i])
+        coord.join(threads)
