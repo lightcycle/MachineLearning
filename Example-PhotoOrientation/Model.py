1a6d1b35421077a0c873ec64d250e6dcee36d3bf
Mike Davis <mike@lightcycle.org>
2016-09-05 08:23:37 -0500

    Split and refactor layer helper functions.

--- Example-PhotoOrientation/Model.py
+++ Example-PhotoOrientation/Model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from operator import mul
 
 
 class Model:
@@ -14,48 +15,60 @@ class Model:
         return tf.Variable(initial)
 
     @classmethod
-    def __conv2d(cls, x, W):
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    def __conv_layer(cls, input_shape, input, k_width, k_height, k_depth, num_outputs):
+        weight = cls.__weight_variable([k_width, k_height, k_depth, num_outputs])
+        bias = cls.__bias_variable([num_outputs])
+        return tf.nn.conv2d(input, weight, strides=[1, 1, 1, 1], padding='SAME') + bias,\
+               (input_shape[0], input_shape[1], num_outputs)
 
     @classmethod
-    def __max_pool(cls, x, size):
-        return tf.nn.max_pool(x, ksize=[1, size, size, 1], strides=[1, size, size, 1], padding='SAME')
+    def __relu_layer(cls, input_shape, input):
+        return tf.nn.relu(input),\
+               input_shape
 
     @classmethod
-    def __conv_layer(cls, input_shape, input, k_width, k_height, k_depth, num_outputs, max_pool):
-        W_conv1 = cls.__weight_variable([k_width, k_height, k_depth, num_outputs])
-        b_conv1 = cls.__bias_variable([num_outputs])
-        h_conv1 = tf.nn.relu(Model.__conv2d(input, W_conv1) + b_conv1)
-        return Model.__max_pool(h_conv1, max_pool), (input_shape[0] / max_pool, input_shape[1] / max_pool, num_outputs)
+    def __maxpool_layer(cls, input_shape, input, pool_size):
+        return tf.nn.max_pool(input, ksize=[1, pool_size, pool_size, 1], strides=[1, pool_size, pool_size, 1], padding='SAME'),\
+               (input_shape[0] / pool_size, input_shape[1] / pool_size, input_shape[2])
 
     @classmethod
     def __fully_connected_layer(cls, input_shape, input, num_outputs):
-        W_fc1 = cls.__weight_variable([input_shape[0] * input_shape[1] * input_shape[2], num_outputs])
-        b_fc1 = cls.__bias_variable([num_outputs])
-        conv_flat = tf.reshape(input, [-1, input_shape[0] * input_shape[1] * input_shape[2]])
-        return tf.nn.relu(tf.matmul(conv_flat, W_fc1) + b_fc1), num_outputs
+        flattened_size = reduce(mul, input_shape, 1)
+        weight = cls.__weight_variable([flattened_size, num_outputs])
+        bias = cls.__bias_variable([num_outputs])
+        input_flat = tf.reshape(input, [-1, flattened_size])
+        return tf.matmul(input_flat, weight) + bias,\
+               (num_outputs,)
 
     @classmethod
-    def __loss_layer(cls, input_shape, input, keep_prob, num_outputs):
-        h_fc1_drop = tf.nn.dropout(input, keep_prob)
-        W_fc2 = cls.__weight_variable([input_shape, num_outputs])
-        b_fc2 = cls.__bias_variable([num_outputs])
-        return tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2), num_outputs
+    def __dropout(cls, input_shape, input, keep_prob):
+        return tf.nn.dropout(input, keep_prob), \
+               input_shape
 
     @classmethod
     def create_graph(cls, input, keep_prob):
-        shape = (200, 200, 3)
-
         # Normalize [0,255] ints to [0,1] floats
         input_float = tf.div(tf.cast(input, tf.float32), 255)
 
         # Build model
         model = input_float
-        model, shape = cls.__conv_layer(shape, model, 5, 5, 3, 8, 2)
-        model, shape = cls.__conv_layer(shape, model, 5, 5, 8, 16, 2)
-        model, shape = cls.__conv_layer(shape, model, 5, 5, 16, 32, 2)
-        model, shape = cls.__conv_layer(shape, model, 5, 5, 32, 64, 5)
+        shape = (200, 200, 3)
+        model, shape = cls.__conv_layer(shape, model, 5, 5, 3, 8)
+        model, shape = cls.__relu_layer(shape, model)
+        model, shape = cls.__maxpool_layer(shape, model, 2)
+        model, shape = cls.__conv_layer(shape, model, 5, 5, 8, 16)
+        model, shape = cls.__relu_layer(shape, model)
+        model, shape = cls.__maxpool_layer(shape, model, 2)
+        model, shape = cls.__conv_layer(shape, model, 5, 5, 16, 32)
+        model, shape = cls.__relu_layer(shape, model)
+        model, shape = cls.__maxpool_layer(shape, model, 2)
+        model, shape = cls.__conv_layer(shape, model, 5, 5, 32, 64)
+        model, shape = cls.__relu_layer(shape, model)
+        model, shape = cls.__maxpool_layer(shape, model, 5)
         model, shape = cls.__fully_connected_layer(shape, model, 128)
-        model, shape = cls.__loss_layer(shape, model, keep_prob, 4)
+        model, shape = cls.__relu_layer(shape, model)
+        model, shape = cls.__dropout(shape, model, keep_prob)
+        model, shape = cls.__fully_connected_layer(shape, model, 4)
+        model, shape = cls.__relu_layer(shape, model)
 
         return model
