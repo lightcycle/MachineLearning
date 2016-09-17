014cfa8eb4a3ea4e1b4a20b52e5e7b0c03ccb6fa
Mike Davis <mike@lightcycle.org>
2016-09-17 12:02:49 -0500

    Write 15 correct and incorrect inference examples.

--- Example-PhotoOrientation/Test.py
+++ Example-PhotoOrientation/Test.py
@@ -27,8 +27,8 @@ correct_prediction = tf.equal(tf.argmax(inferred_labels, 1), tf.argmax(tf.cast(l
 accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 correct_images = tf.boolean_mask(image_batch, correct_prediction)
 incorrect_images = tf.boolean_mask(image_batch, tf.logical_not(correct_prediction))
-tf.image_summary('Correct Inference', correct_images, max_images = 5)
-tf.image_summary('Incorrect Inference', incorrect_images, max_images = 5)
+tf.image_summary('Correct Inference', correct_images, max_images = 20)
+tf.image_summary('Incorrect Inference', incorrect_images, max_images = 20)
 
 # Run graph
 average = Average()
